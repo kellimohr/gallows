e6813937b3fa3b02933092ee480fd5351658c5be
Kelli Mohr <kelli.mohr@nordstrom.com>
2017-12-07 14:12:09 -0800

    Added help file to clean up code.  Added a New Game button when the game is over.

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -1,26 +1,15 @@
 defmodule GallowsWeb.HangmanView do
+  
   use GallowsWeb, :view
 
-  @responses %{
-    :won          => { :success, "We have a winner!" },
-    :lost         => { :danger, "YOU LOSE!" },
-    :good_guess   => { :success, "Correct!" },
-    :bad_guess    => { :warning, "Wrong. So Wrong." },
-    :already_used => { :info, "Hello McFly. You guessed that already!"}
-  }
+  import Gallows.Views.Helpers.GameStateHelper
 
-  def game_state(state) do
-    @responses[state]
-    |> alert()
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [ :won, :lost ]
   end
-   
-  defp alert(nil), do: ""
-  defp alert( {class, message} ) do
-    """
-    <div class="alert alert-#{class}">
-      #{message} 
-    </div>
-    """
-    |> raw()
+  
+  def new_game_button(conn) do
+    button("New Game", to: hangman_path(conn, :create_game)) 
   end
+
 end
---
+++ lib/gallows_web/views/helpers/game_state_helper.ex
@@ -0,0 +1,27 @@
+defmodule Gallows.Views.Helpers.GameStateHelper do
+    
+  import Phoenix.HTML, only: [ raw: 1 ] 
+
+  @responses %{
+    :won          => { :success, "We have a winner!" },
+    :lost         => { :danger, "YOU LOSE!" },
+    :good_guess   => { :success, "Correct!" },
+    :bad_guess    => { :warning, "Wrong. So Wrong." },
+    :already_used => { :info, "Hello McFly. You guessed that already!"}
+  }
+    
+  def game_state(state) do
+    @responses[state]
+    |> alert()
+  end
+       
+  defp alert(nil), do: ""
+  defp alert( {class, message} ) do
+    """
+    <div class="alert alert-#{class}">
+      #{message} 
+      </div>
+      """
+      |> raw()
+  end
+end
